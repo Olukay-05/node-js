9377057cd50265b989ed9186ce40f19e3329bd5e
Olukayode Ogunnowo <olukayode.ogunnowo@yahoo.com>
2024-11-10 09:14:05 +0100

    Learnt about Asyncronous method of using the file system module

--- starter/index.js
+++ starter/index.js
@@ -1,5 +1,44 @@
 const fs = require("fs");
+const http = require("http");
+const url = require("url");
 
-const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
+// Blocking, Synchronous method
+// const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
+// console.log(textIn);
 
-console.log(textIn);
+// const textOutput = `This is what we know about the Avocado: ${textIn}.\nCreated on ${Date.now()}`;
+
+// fs.writeFileSync("./txt/output.txt", textOutput);
+// console.log("Text has been created ");
+
+// Non - Blocking, Asynchronous method
+// fs.readFile("./txt/start.txt", "utf-8", (err, data) => {
+//   console.log(data);
+// });
+
+//Server
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
+
+const dataObj = JSON.parse(data);
+
+const server = http.createServer((req, res) => {
+  const pathname = req.url;
+  if (pathname === "/" || pathname === "/overview") {
+    res.end("this is the overvoew page");
+  } else if (pathname === "/product") {
+    res.end("this is the product page");
+  } else if (pathname === "/api") {
+    res.writeHead(200, { "Content-type": "application/json" });
+    res.end(data);
+  } else {
+    res.writeHead(404, {
+      "Content-type": "text/html",
+    });
+    res.end("<h1>Page not found</>");
+  }
+});
+
+server.listen(8000, "127.0.0.1", () => {
+  console.log("Listening to requests on port 8000");
+});
