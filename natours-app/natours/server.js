0e15759a4458b3a4e7eb7a7ea664624c89a791f4
Olukayode Ogunnowo <olukayode.ogunnowo@yahoo.com>
2025-01-08 19:20:12 +0100

    Implemented auth controller, user signup and user login

--- natours-app/natours/server.js
+++ natours-app/natours/server.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+
 const app = require('./app');
 
 dotenv.config({ path: './config.env' });
@@ -21,6 +22,14 @@ mongoose
   });
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`App running on port ${port}...`);
+});
+
+process.on('unhandledRejection', (err) => {
+  console.log('UNHANDLED REJECTION! 💥 Shutting down...');
+  console.log(err.name, err.message);
+  server.close(() => {
+    process.exit(1);
+  });
 });
